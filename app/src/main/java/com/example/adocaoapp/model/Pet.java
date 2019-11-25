e245af5ab54f3cf34f271bdd22c909350acddb8a
felipembfelipe <felipembfelipe@gmail.com>
2019-11-25 14:13:17 -0300

    API Atualizada

--- app/src/main/java/com/example/adocaoapp/model/Pet.java
+++ app/src/main/java/com/example/adocaoapp/model/Pet.java
@@ -14,6 +14,8 @@ public class Pet {
     private String cor;
     private String sexo;
     private String descricao;
+    private Boolean favorito;
+    private Boolean adotado;
 
     public int getId() {
         return id;
@@ -57,5 +59,13 @@ public class Pet {
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public Boolean getFavorito(){
+        return favorito;
+    }
+
+    public Boolean getAdotado(){
+        return adotado;
     }
 }
